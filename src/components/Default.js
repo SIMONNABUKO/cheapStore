ba1ac290e1f11edb2a24580d2f2a97e13307e2e5
Simon Angatia <simonnabuko@gmail.com>
2020-03-20 15:19:31 +0300

    Setting up create react project, setting up components, setting up context api, styling productList and Product components

---
+++ src/components/Default.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Default extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Ooops! Page not found</h2>
+      </div>
+    );
+  }
+}
+
+export default Default;
